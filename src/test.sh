d8d4288d76acb8c1ffb882c9434b0fb0fb56b10c
mbinary <zhuheqin1@mail.com>
2018-05-13 10:57:13 +0800

    :smiley:

--- src/test.sh
+++ src/test.sh
@@ -1,9 +1,16 @@
 gcc -D_FILE_OFFSET_BITS=64 -o oshfs oshfs.c -lfuse 
+
+set -x # debug on
+
 cd mountpoint
 ls -al
-echo helloworld > testfile
+touch test
+mv test testfile
 ls -l testfile # 查看是否成功创建文件
+echo hello > testfile
 cat testfile # 测试写入和读取是否相同
+chmod 777 testfile && chown mbinary  testfile 
+ls -l testfile
 dd if=/dev/zero of=testfile bs=1M count=2000
 ls -l testfile # 测试2000MiB大文件写入
 dd if=/dev/urandom of=testfile bs=1M count=1 seek=10
@@ -11,3 +18,5 @@ ls -l testfile # 此时应为11MiB
 dd if=testfile of=/dev/null # 测试文件读取
 rm testfile
 ls -al # testfile是否成功删除
+
+set +x # debug off
